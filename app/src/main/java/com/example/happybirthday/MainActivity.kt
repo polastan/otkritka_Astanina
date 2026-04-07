ef88a607e18ec53a33b7ba8a21ee6c69867a86ed
polastan <astanina0708@mail.ru>
2026-04-07 19:38:16 +0300

    Edit background image

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -76,6 +76,9 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
         Image(
             painter = image,
             contentDescription = null,
+            modifier = Modifier. fillMaxSize(),
+            contentScale = ContentScale. Crop,
+            alpha = 0.5F
 
         )
         GreetingText(
